89c8a6a38e81a43828bf03c731f5992486a5b32f
Ryan B <158354470+Warckoooooo@users.noreply.github.com>
2024-06-15 22:17:38 +0200

    Update update_age.sh

--- .github/workflows/update_age.sh
+++ .github/workflows/update_age.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Ton anniversaire (format: MMDDYY)
+# Ton anniversaire (format: MMDDYYYY)
 BIRTHDATE="09022002"
 
-# Recupère la date d'aujourd'hui
-TODAY=$(date +%Y%m%d)
+# Récupère la date d'aujourd'hui (format: MMDDYYYY)
+TODAY=$(date +%m%d%Y)
 
-# Calcule le nombre de jour depuis ton anniversaire
-DAYS=$(($(($(date -d"$TODAY" +%s) - $(date -d"$BIRTHDATE" +%s))) / 86400))
+# Calcule le nombre de jours depuis ton anniversaire
+DAYS=$(($(($(date -d "$TODAY" +%s) - $(date -d "$BIRTHDATE" +%s))) / 86400))
 
-# Replace l'âge dans le fichier README
-sed -i "s/I'm Ryan/I'm Ryan - ${DAYS} days old/1" README.md
+# Remplace l'âge dans le fichier README
+sed -i "s/Hi 👋, I'm Ryan/Hi 👋, I'm Ryan - ${DAYS} days old/1" README.md
