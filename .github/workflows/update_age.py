624bf19411b462b7bde6e38afec3074b5230436e
bouard603 <bouard603@gmail.com>
2024-07-28 12:05:33 +0000

    update acions

--- .github/workflows/update_age.py
+++ .github/workflows/update_age.py
@@ -1,19 +1,28 @@
 import datetime
 
-# Your birthdate
-birthdate = datetime.date(2002, 09, 02)
+# Ma date de naissance
+birthdate = datetime.date(2002, 9, 2)
 
-# Calculate the number of days since birth
+# Calculer le nombre de jours depuis la naissance
 today = datetime.date.today()
 days_old = (today - birthdate).days
 
-# Read the README.md file
-with open("README.md", "r") as file:
+# Lire le fichier README.md
+readme_path = "README.md"
+with open(readme_path, "r", encoding="utf-8") as file:
     readme_content = file.read()
 
-# Replace the age placeholder with the actual age
-updated_readme_content = readme_content.replace("I'm Ryan -  days old", f"I'm Ryan - {days_old} days old")
+# La ligne de base à rechercher
+base_line = '<h1 align="center">Hi 👋, I\'m Ryan'
+new_content = f'{base_line} ({days_old} days old)'
 
-# Write the updated content back to README.md
-with open("README.md", "w") as file:
-    file.write(updated_readme_content)
+# Remplacer ou ajouter l'âge en jours
+if base_line in readme_content:
+    # Trouver la ligne et la remplacer
+    updated_readme_content = readme_content.replace(base_line, new_content)
+    
+    # Écrire le contenu mis à jour dans README.md
+    with open(readme_path, "w", encoding="utf-8") as file:
+        file.write(updated_readme_content)
+else:
+    print("La ligne de base n'a pas été trouvée dans le fichier README.md.")
